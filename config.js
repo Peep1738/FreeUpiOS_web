52a63ece032d515b73213a8896ef8a5f1aa64730
Sky1738 <87045314+Peep1738@users.noreply.github.com>
2026-04-06 16:36:15 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+/* ==========================================
+   FreeUp iOS - サイト共通設定
+   ========================================== */
+const CONFIG = {
+    // GitHub連携 (更新履歴・IPA取得用)
+    githubOwner: '1738-sky',              // あなたのGitHubユーザー名
+    githubRepo: 'FreeupiOS_web',          // Releasesを発行するリポジトリ名
+
+    // 外部リンク
+    kofiUrl: 'https://ko-fi.com/YOUR_KOFI_LINK', // Ko-fiのURL
+    xUrl: 'https://x.com/1738_sky',              // 𝕏 (Twitter)のURL
+    shortcutUrl: 'https://www.icloud.com/shortcuts/b6e9fec203244dc48425c6f05421f326' // iCloudショートカットのURL
+};
