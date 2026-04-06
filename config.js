c14c5d80eedea474ab7be3662e7c75a19bdda501
Sky1738 <87045314+Peep1738@users.noreply.github.com>
2026-04-06 18:11:19 +0900

    config.js を更新

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     githubRepo: 'FreeUpiOS_web',          // Releasesを発行するリポジトリ名
 
     // 外部リンク
-    kofiUrl: 'https://ko-fi.com/YOUR_KOFI_LINK', // Ko-fiのURL
+    kofiUrl: 'https://ko-fi.com/s/5fc953fc97', // Ko-fiのURL
     xUrl: 'https://x.com/1738_sky',              // 𝕏 (Twitter)のURL
     shortcutUrl: 'https://www.icloud.com/shortcuts/b6e9fec203244dc48425c6f05421f326' // iCloudショートカットのURL
 };
